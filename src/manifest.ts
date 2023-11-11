aa85271f893f285d525d971d2b3a184e0d70ee03
jemmec <spogjem@gmail.com>
2023-11-11 13:17:52 +1030

    Removed things I dont need

--- src/manifest.ts
+++ src/manifest.ts
@@ -17,20 +17,16 @@ export default defineManifest({
     default_icon: 'img/logo-48.png',
   },
   options_page: 'options.html',
-  devtools_page: 'devtools.html',
   background: {
     service_worker: 'src/background/index.ts',
     type: 'module',
   },
   content_scripts: [
     {
-      matches: ['http://*/*', 'https://*/*'],
+      matches: ['http://www.moxfield.com/*', 'https://www.moxfield.com/*'],
       js: ['src/contentScript/index.ts'],
     },
   ],
-  side_panel: {
-    default_path: 'sidepanel.html',
-  },
   web_accessible_resources: [
     {
       resources: ['img/logo-16.png', 'img/logo-34.png', 'img/logo-48.png', 'img/logo-128.png'],
@@ -38,7 +34,4 @@ export default defineManifest({
     },
   ],
   permissions: ['sidePanel', 'storage'],
-  chrome_url_overrides: {
-    newtab: 'newtab.html',
-  },
 })
