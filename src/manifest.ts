425fd0f1364912d15c1b5642d3b35d5b5359846e
jemmec <spogjem@gmail.com>
2023-11-11 13:21:43 +1030

    Update readme

--- src/manifest.ts
+++ src/manifest.ts
@@ -16,7 +16,7 @@ export default defineManifest({
     default_popup: 'popup.html',
     default_icon: 'img/logo-48.png',
   },
-  options_page: 'options.html',
+  // options_page: 'options.html',
   background: {
     service_worker: 'src/background/index.ts',
     type: 'module',
